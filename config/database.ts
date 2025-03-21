9fea794d9d5abf0fa2e47f2b6367101c420e54a7
Juan Camilo Gutierrez Fuentes <juan.gutierrez@softgic.co>
2025-03-21 16:21:01 -0500

    ajustes conexion mysql

--- config/database.ts
+++ config/database.ts
@@ -1,20 +1,13 @@
-import path from 'path';
-
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'mysql2', // Cambia de 'mysql' a 'mysql2'
     connection: {
-      host: env('DATABASE_HOST', 'mysql'), // Nombre del servicio en docker-compose
+      host: env('DATABASE_HOST', 'mysql'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'dbprueba1'), // Nombre de la BD en docker-compose
-      user: env('DATABASE_USERNAME', 'usermysql1'), // Usuario en docker-compose
-      password: env('DATABASE_PASSWORD', '123456'), // Contraseña en docker-compose
-      ssl: false, // No necesitas SSL en este caso
+      database: env('DATABASE_NAME', 'dbprueba1'),
+      user: env('DATABASE_USERNAME', 'usermysql1'),
+      password: env('DATABASE_PASSWORD', '123456'),
+      ssl: false,
     },
-    pool: {
-      min: env.int('DATABASE_POOL_MIN', 2),
-      max: env.int('DATABASE_POOL_MAX', 10),
-    },
-    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
   },
 });
